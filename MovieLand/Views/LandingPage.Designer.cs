6ea53907b80b47f861b0c0730cf3f546d0a42b79
adrian <arizqynaya@gmail.com>
2025-06-04 15:15:10 +0700

    Add project files.

---
+++ MovieLand/Views/LandingPage.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace MovieLand.Views
+{
+    partial class LandingPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // LandingPage
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1924, 1055);
+            Name = "LandingPage";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "LandingPage";
+            Load += LandingPage_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ MovieLand/Views/LandingPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieLand.Views
+{
+    public partial class LandingPage : Form
+    {
+        public LandingPage()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void LandingPage_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
