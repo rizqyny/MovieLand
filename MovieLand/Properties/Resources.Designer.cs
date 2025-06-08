fee1d6c222d31abf436f9f6c143236d5f80dba4b
AdenGtgBgt <adenperdana1477@gmail.com>
2025-06-08 16:22:15 +0700

    p

--- MovieLand/Properties/Resources.Designer.cs
+++ MovieLand/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace MovieLand.Properties {
                 return ((byte[])(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap MovieLand__1_ {
+            get {
+                object obj = ResourceManager.GetObject("MovieLand (1)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
--- MovieLand/Views/DataDiri.Designer.cs
+++ MovieLand/Views/DataDiri.Designer.cs
@@ -32,59 +32,53 @@
             tbAlamat = new TextBox();
             label1 = new Label();
             label2 = new Label();
-            label3 = new Label();
             btnSimpan = new Button();
             SuspendLayout();
             // 
             // tbNama_lengkap
             // 
-            tbNama_lengkap.Location = new Point(102, 162);
+            tbNama_lengkap.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tbNama_lengkap.Location = new Point(732, 254);
             tbNama_lengkap.Name = "tbNama_lengkap";
-            tbNama_lengkap.Size = new Size(341, 27);
+            tbNama_lengkap.Size = new Size(459, 34);
             tbNama_lengkap.TabIndex = 0;
             tbNama_lengkap.TextChanged += tbNama_lengkap_TextChanged;
             // 
             // tbAlamat
             // 
-            tbAlamat.Location = new Point(102, 260);
+            tbAlamat.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tbAlamat.Location = new Point(732, 438);
             tbAlamat.Name = "tbAlamat";
-            tbAlamat.Size = new Size(341, 27);
+            tbAlamat.Size = new Size(459, 34);
             tbAlamat.TabIndex = 1;
             tbAlamat.TextChanged += tbAlamat_TextChanged;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(102, 139);
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(732, 223);
             label1.Name = "label1";
-            label1.Size = new Size(109, 20);
+            label1.Size = new Size(143, 28);
             label1.TabIndex = 2;
             label1.Text = "Nama Lengkap";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(102, 237);
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(732, 407);
             label2.Name = "label2";
-            label2.Size = new Size(57, 20);
+            label2.Size = new Size(74, 28);
             label2.TabIndex = 3;
             label2.Text = "Alamat";
             // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label3.Location = new Point(102, 63);
-            label3.Name = "label3";
-            label3.Size = new Size(151, 41);
-            label3.TabIndex = 4;
-            label3.Text = "DATA DIRI";
-            // 
             // btnSimpan
             // 
-            btnSimpan.Location = new Point(632, 228);
+            btnSimpan.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSimpan.Location = new Point(1084, 572);
             btnSimpan.Name = "btnSimpan";
-            btnSimpan.Size = new Size(94, 29);
+            btnSimpan.Size = new Size(107, 40);
             btnSimpan.TabIndex = 5;
             btnSimpan.Text = "Simpan";
             btnSimpan.UseVisualStyleBackColor = true;
@@ -94,9 +88,9 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackgroundImage = Properties.Resources.MovieLand__1_;
+            ClientSize = new Size(1532, 981);
             Controls.Add(btnSimpan);
-            Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(tbAlamat);
@@ -114,7 +108,6 @@
         private TextBox tbAlamat;
         private Label label1;
         private Label label2;
-        private Label label3;
         private Button btnSimpan;
     }
 }
