af0d3916ba8f1d906d910a3a59f384e75c12f3b3
adrian <arizqynaya@gmail.com>
2025-06-14 12:41:30 +0700

    p

--- MovieLand/Views/Transaksi.Designer.cs
+++ MovieLand/Views/Transaksi.Designer.cs
@@ -32,21 +32,13 @@
             pictureBox1 = new PictureBox();
             label1 = new Label();
             button1 = new Button();
-            pictureBox2 = new PictureBox();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
-            label6 = new Label();
-            label7 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(689, 73);
+            pictureBox1.Location = new Point(385, 93);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(262, 260);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -56,7 +48,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(777, 354);
+            label1.Location = new Point(472, 374);
             label1.Name = "label1";
             label1.Size = new Size(113, 20);
             label1.TabIndex = 1;
@@ -64,96 +56,27 @@
             // 
             // button1
             // 
-            button1.Location = new Point(786, 407);
+            button1.Location = new Point(481, 425);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 2;
             button1.Text = "Selesai";
             button1.UseVisualStyleBackColor = true;
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.Location = new Point(22, 37);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(147, 181);
-            pictureBox2.TabIndex = 3;
-            pictureBox2.TabStop = false;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(200, 37);
-            label2.Name = "label2";
-            label2.Size = new Size(63, 25);
-            label2.TabIndex = 4;
-            label2.Text = "label2";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(200, 73);
-            label3.Name = "label3";
-            label3.Size = new Size(50, 20);
-            label3.TabIndex = 5;
-            label3.Text = "label3";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(200, 135);
-            label4.Name = "label4";
-            label4.Size = new Size(95, 20);
-            label4.TabIndex = 6;
-            label4.Text = "Nomor Kursi:";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(200, 104);
-            label5.Name = "label5";
-            label5.Size = new Size(50, 20);
-            label5.TabIndex = 7;
-            label5.Text = "label5";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label6.Location = new Point(200, 176);
-            label6.Name = "label6";
-            label6.Size = new Size(59, 23);
-            label6.TabIndex = 8;
-            label6.Text = "label6";
-            // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.Location = new Point(291, 135);
-            label7.Name = "label7";
-            label7.Size = new Size(50, 20);
-            label7.TabIndex = 9;
-            label7.Text = "label7";
+            button1.Click += button1_Click;
             // 
             // Transaksi
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(982, 503);
-            Controls.Add(label7);
-            Controls.Add(label6);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(pictureBox2);
             Controls.Add(button1);
             Controls.Add(label1);
             Controls.Add(pictureBox1);
             Name = "Transaksi";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Transaksi";
+            Load += Transaksi_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -163,12 +86,5 @@
         private PictureBox pictureBox1;
         private Label label1;
         private Button button1;
-        private PictureBox pictureBox2;
-        private Label label2;
-        private Label label3;
-        private Label label4;
-        private Label label5;
-        private Label label6;
-        private Label label7;
     }
 }
--- MovieLand/Views/Transaksi.cs
+++ MovieLand/Views/Transaksi.cs
@@ -7,14 +7,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace MovieLand.Views
 {
-    public partial class Transaksi: Form
+    public partial class Transaksi : Form
     {
-        public Transaksi()
+        private int _idCustomer, _idFilm, _nomorKursi;
+
+        public Transaksi(int idCustomer, int idFilm, int nomorKursi)
         {
             InitializeComponent();
+            _idCustomer = idCustomer;
+            _idFilm = idFilm;
+            _nomorKursi = nomorKursi;
+
+            // (Opsional) tampilkan informasi di label kalau ingin
+            // labelInfo.Text = $"Film ID: {_idFilm}, Kursi: {_nomorKursi}";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var conn = Database.GetConnection())
+            {
+                conn.Open();
+
+                using (var trans = conn.BeginTransaction())
+                {
+                    try
+                    {
+                        // 1. Insert ke tabel transaksi
+                        string insertQuery = @"
+                        INSERT INTO transaksi (waktu, id_customer, nomor_kursi, id_film)
+                        VALUES (CURRENT_TIMESTAMP, @idCustomer, @nomorKursi, @idFilm)";
+
+                        using (var cmd = new NpgsqlCommand(insertQuery, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@idCustomer", _idCustomer);
+                            cmd.Parameters.AddWithValue("@nomorKursi", _nomorKursi);
+                            cmd.Parameters.AddWithValue("@idFilm", _idFilm);
+                            cmd.ExecuteNonQuery();
+                        }
+
+                        // 2. Update status kursi
+                        string updateQuery = @"
+                        UPDATE kursi SET status = 'Tidak Tersedia'
+                        WHERE nomor_kursi = @nomorKursi";
+
+                        using (var cmd = new NpgsqlCommand(updateQuery, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@nomorKursi", _nomorKursi);
+                            cmd.ExecuteNonQuery();
+                        }
+
+                        trans.Commit();
+                        MessageBox.Show("Transaksi berhasil disimpan!");
+                        this.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        trans.Rollback();
+                        MessageBox.Show("Terjadi kesalahan: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void Transaksi_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
